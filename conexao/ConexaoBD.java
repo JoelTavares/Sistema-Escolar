26cd4fbc0d016c29ddd126da03639c7b0de77d5a
JDT <joel_jdt@hotmail.com>
2015-03-12 16:26:20 -0300

    Alterado!

--- conexao/ConexaoBD.java
+++ conexao/ConexaoBD.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package ifrn.edu.conexao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
